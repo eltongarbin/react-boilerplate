c9aa1c5a2ce26ae9c60e931b2899898457010683
Elton Garbin <eltongarbin@gmail.com>
2018-12-29 19:53:30 -0200

    suport proposed javascript features with babel plugins

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = { count: 0 };
+
+  handleToggleCountClick = (mode) => () => {
+    this.setState((prevState) => ({
+      count: mode === 'increase' ? prevState.count + 1 : prevState.count - 1
+    }));
+  };
+
   render() {
-    return <h1>Hello world!</h1>;
+    return (
+      <div>
+        <h1>Hello world!</h1>
+        <h2>Count: {this.state.count}</h2>
+        <button onClick={this.handleToggleCountClick('increase')}>+</button>
+        <button onClick={this.handleToggleCountClick('decrease')}>-</button>
+      </div>
+    );
   }
 }
 
